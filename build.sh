baa8af2557c00ae23fb0dfefdac3a0b6424d1870
Russell Klopfer <russell@klopfer.us>
2017-08-29 14:24:49 -0400

    separating download and build scripts

--- build.sh
+++ build.sh
@@ -1,35 +1,5 @@
 #!/usr/bin/env bash
 
-# tensorflow revision
-if ! [ $TF_REV ]; then
-    TF_REV=1.3.0-rc1
-    echo "Defaulting to TF_REV=${TF_REV}"
-fi
-
-# native type
-if ! [ $TF_TYPE ]; then
-    TF_TYPE="gpu"
-    echo "Defaulting to TF_TYPE=${TF_TYPE}"
-fi
-
-# OS
-OS=$(uname -s | tr '[:upper:]' '[:lower:]')
-
-JNI_URL="https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow_jni-${TF_TYPE}-${OS}-x86_64-${TF_REV}.tar.gz"
-JAR_URL="https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_REV}.jar"
-
-# clean
-rm -rvf jni/
-rm -vf libtensorflow.jar
-mkdir ./jni
-
-echo "Downloading: ${JNI_URL}"
-curl -L ${JNI_URL} |
-tar --warning=no-timestamp -xz -C ./jni
-
-echo "Downloading: ${JAR_URL}"
-curl -o libtensorflow.jar ${JAR_URL}
-
 # build
 [ -d out ] || mkdir out
 javac -cp libtensorflow.jar -d out/ test/MemoryTest.java 
---
+++ resolve.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# tensorflow revision
+if ! [ $TF_REV ]; then
+    TF_REV=1.3.0-rc1
+    echo "Defaulting to TF_REV=${TF_REV}"
+fi
+
+# native type
+if ! [ $TF_TYPE ]; then
+    TF_TYPE="gpu"
+    echo "Defaulting to TF_TYPE=${TF_TYPE}"
+fi
+
+# OS
+OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+
+JNI_URL="https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow_jni-${TF_TYPE}-${OS}-x86_64-${TF_REV}.tar.gz"
+JAR_URL="https://storage.googleapis.com/tensorflow/libtensorflow/libtensorflow-${TF_REV}.jar"
+
+# clean
+rm -rvf jni/
+rm -vf libtensorflow.jar
+mkdir ./jni
+
+echo "Downloading: ${JNI_URL}"
+curl -L ${JNI_URL} |
+tar --warning=no-timestamp -xz -C ./jni
+
+echo "Downloading: ${JAR_URL}"
+curl -o libtensorflow.jar ${JAR_URL}
